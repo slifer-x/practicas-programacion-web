c85342abbb3af87fc653049daa14a6dae1d184f3
Manuel <manuelolmedo18@gmail.com>
2012-10-31 13:42:06 -0600

    Se agrega practica 19

---
+++ Practica 19/js/valida.js
@@ -0,0 +1,51 @@
+/**
+ * @author Manuel Alejandro Meza Olmedo
+ */
+
+	
+function valida() {
+	var cont = 0;
+	
+	valor = document.registro.nick.value;
+	if(!/\w{4,20}/.test(valor) || valor == '') {
+		document.getElementById("error_nick").style.visibility='visible';
+	} else {
+		document.getElementById("error_nick").style.visibility='hidden';
+		cont++;
+	}
+	
+	valor = document.registro.correo.value;
+	if(!/[a-z_0-9.]+@[a-z_0-9]{4,}.[a-z]{2,4}$/.test(valor)) {
+		document.getElementById("error_mail").style.visibility='visible';
+	} else {
+		document.getElementById("error_mail").style.visibility='hidden';
+		cont++;
+	}
+	
+	valor = document.registro.passwd.value;
+	if(!/[A-Za-z0-9-_.,]+$/.test(valor)) {
+		document.getElementById("error_passwd").style.visibility='visible';
+	} else {
+		document.getElementById("error_passwd").style.visibility='hidden';
+		cont++;
+	}
+	
+	valor = document.registro.passwd.value;
+	if(valor != document.getElementById("repasswd").value) {
+		document.getElementById("error_repasswd").style.visibility='visible';
+		document.getElementById("repasswd").value = '';
+	} else {
+		document.getElementById("error_repasswd").style.visibility="hidden";
+		cont++;
+	}
+	
+	
+	
+	if(cont == 4) {
+		document.getElementById("md5").value = md5(valor);
+		document.getElementById("passwd").value = '';
+		document.getElementById("repasswd").value = '';
+		
+		document.registro.submit.click();
+	}
+}
